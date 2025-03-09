726732a9c830283dc382573e8cdbcb220622a3ea
Naughty1029 <masahiro.watanabe02@gmail.com>
2025-03-09 15:10:32 +0900

    test と it を利用したテストケースの作成:

---
+++ src/chapter2/assertion/assertion.test.ts
@@ -0,0 +1,11 @@
+test("testを利用してテストケースを作成する",()=> {
+  const result = true;
+  const expected = true;
+  expect(result).toBe(expected);
+})
+
+it("itを利用してテストケースを作成する",()=> {
+  const result = true;
+  const expected = true;
+  expect(result).toBe(expected);
+})
