0c84e172e94e1644ab904290ec03b6bbfe6cf837
Naughty1029 <masahiro.watanabe02@gmail.com>
2025-03-09 15:19:56 +0900

    プリミティブな値の比較

--- src/chapter2/assertion/assertion.test.ts
+++ src/chapter2/assertion/assertion.test.ts
@@ -9,3 +9,26 @@ it("itを利用してテストケースを作成する",()=> {
   const expected = true;
   expect(result).toBe(expected);
 })
+
+//プリミティブな値の比較
+const numberValue = 0;
+const stringValue = "文字列";
+const booleanValue = true;
+
+test("toBeでプリミティブな値を比較する",()=> {
+  expect(numberValue).toBe(0);
+  expect(stringValue).toBe("文字列");
+  expect(booleanValue).toBe(true);
+})
+
+test("toEqualでプリミティブな値を比較する",()=> {
+  expect(numberValue).toEqual(0);
+  expect(stringValue).toEqual("文字列");
+  expect(booleanValue).toEqual(true);
+})
+
+test("toStrictEqualでプリミティブな値を比較する",()=> { 
+  expect(numberValue).toStrictEqual(0);
+  expect(stringValue).toStrictEqual("文字列");
+  expect(booleanValue).toStrictEqual(true);
+})
