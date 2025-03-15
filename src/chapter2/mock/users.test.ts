f17b2d49e5e38302d678fc58910d8ecbc8e47d0f
Naughty1029 <masahiro.watanabe02@gmail.com>
2025-03-15 17:48:21 +0900

    外部モジュールをmock化

--- src/chapter2/mock/users.test.ts
+++ src/chapter2/mock/users.test.ts
@@ -3,11 +3,11 @@ import Users from "./users";
 
 jest.mock("axios");
 
-test("should fetch all users", async() => {
-  const users = [{name: "Bob"}];
+test("should fetch users", async() => {
+  const users = [{
+    name: "Bob",
+  }]
   const resp = { data: users };
-  (axios as jest.Mocked<typeof axios>).get.mockResolvedValue(resp);
-  // axios.get.mockImplementation(() => Promise.resolve(resp)) // 上記のmockResolvedValueと同じ設定
-
+  (axios.get as jest.Mock).mockResolvedValue(resp);
   await expect(Users.search()).resolves.toEqual(users);
-})
+});
